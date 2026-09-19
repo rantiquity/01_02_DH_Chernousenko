64a20efab8c9e9755fe1da209319788a202292aa
Sofya Chernousenko <113456160+Carasinaa@users.noreply.github.com>
2026-09-19 15:52:41 +0300

    Update assignment.R

--- assignment.R
+++ assignment.R
@@ -18,7 +18,7 @@ my_data <- works |>
 # Удалите столбец name
 my_data2 <- my_data |>
   # ваш код здесь
-  separate(author, c('author', 'name'), sep=',') |>
+  separate(author, c('author', 'name'), sep=', ') |>
   select(-name)
 
 
